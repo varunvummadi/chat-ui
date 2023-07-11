8918af7845b8d311691e0c0f7e8985af7937b9ac
varunvummadi <varunvummadi@iitkgp.ac.in>
2023-07-12 03:59:44 +0530

    changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-node";
+//import adapter from "@sveltejs/adapter-node";
+import { adapter } from 'sveltekit-adapter-aws';
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import dotenv from "dotenv";
 
